b9293845b0406249a76ae4115fc8e31728de5a43
Christine Nwaigwe <143651871+ChristineNwaigwe@users.noreply.github.com>
2026-03-17 13:53:31 -0500

    finished with CheckBankAccount

---
+++ src/main/CheckBankAccount.java
@@ -0,0 +1,16 @@
+package main;
+
+public class CheckBankAccount {
+
+    private double balance;
+
+    public CheckBankAccount(double balance) {
+        this.balance = balance;
+
+    }
+
+    public double checkBalance() {
+        return balance;
+    }
+
+}
