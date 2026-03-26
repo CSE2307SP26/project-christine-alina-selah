4c8aa25d8ad1bd4ebecf070d6133bb01eca581b0
selahpabon <p.selah@wustl.edu>
2026-03-26 02:00:23 -0500

    Added AccountCreation class and tests for the creating a new account task (number 5)

--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -1,33 +1,49 @@
 package main;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class MainMenu {
 
     private static final int EXIT_SELECTION = 2;
-	private static final int MAX_SELECTION = 2;
+    private static final int MAX_SELECTION = 5;
 
-	private BankAccount userAccount;
+    private BankAccount userAccount;
     private Scanner keyboardInput;
+
+    // Adding hashmap in order to keep track of multiple bank accounts
+    private HashMap<String, BankAccount> accounts;
+    private String currentAccountName;
+    private AccountCreation accountCreation;
 
     public MainMenu() {
         this.userAccount = new BankAccount();
         this.keyboardInput = new Scanner(System.in);
+
+        // Constructing hashmap, setting default bank account, and adding said bank
+        // account to hashmap
+        this.accounts = new HashMap<>();
+        this.currentAccountName = "default";
+        this.accounts.put(this.currentAccountName, this.userAccount);
+
+        this.accountCreation = new AccountCreation();
     }
 
     public void displayOptions() {
         System.out.println("Welcome to the 237 Bank App!");
-        
+
         System.out.println("1. Make a deposit");
         System.out.println("2. Exit the app");
 
+        System.out.println("5. Create a new account");
     }
 
     public int getUserSelection(int max) {
         int selection = -1;
-        while(selection < 1 || selection > max) {
+        while (selection < 1 || selection > max) {
             System.out.print("Please make a selection: ");
             selection = keyboardInput.nextInt();
+            keyboardInput.nextLine();
         }
         return selection;
     }
@@ -36,21 +52,50 @@ public class MainMenu {
         switch (selection) {
             case 1:
                 performDeposit();
+                break;
+            case 5:
+                performCreateAccount();
+                break;
         }
     }
 
     public void performDeposit() {
         double depositAmount = -1;
-        while(depositAmount < 0) {
+        while (depositAmount < 0) {
             System.out.print("How much would you like to deposit: ");
             depositAmount = keyboardInput.nextInt();
         }
         userAccount.deposit(depositAmount);
     }
 
+    // Method to create a new account
+    public void performCreateAccount() {
+        try {
+            // Prompting user for account name
+            System.out.println("What would you like to name your new account?");
+            String accountName = keyboardInput.nextLine();
+
+            // Prompting user for initial account balance
+            System.out.print("How much would you like to deposit: ");
+            double initialBalance = keyboardInput.nextDouble();
+            keyboardInput.nextLine();
+
+            BankAccount newAccount = accountCreation.createAccount(accountName, initialBalance, accounts);
+
+            this.userAccount = newAccount;
+            this.currentAccountName = accountName;
+
+            System.out.println(
+                    "You are now using the " + accountName + " account with a balance of $" + newAccount.getBalance());
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public void run() {
         int selection = -1;
-        while(selection != EXIT_SELECTION) {
+        while (selection != EXIT_SELECTION) {
             displayOptions();
             selection = getUserSelection(MAX_SELECTION);
             processInput(selection);
@@ -61,5 +106,4 @@ public class MainMenu {
         MainMenu bankApp = new MainMenu();
         bankApp.run();
     }
-
 }
