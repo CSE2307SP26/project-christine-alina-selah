85bec25ee8ac3072b3ffde31940e04ed65353bdd
Christine Nwaigwe <143651871+ChristineNwaigwe@users.noreply.github.com>
2026-04-06 16:42:04 -0500

    i added viewtransactionhistory to the main menu

--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -4,28 +4,30 @@ import java.util.Scanner;
 
 public class MainMenu {
 
-    private static final int EXIT_SELECTION = 2;
-	private static final int MAX_SELECTION = 2;
+    private static final int EXIT_SELECTION = 3;
+    private static final int MAX_SELECTION = 3;
 
-	private BankAccount userAccount;
+    private BankAccount userAccount;
     private Scanner keyboardInput;
+    private ViewTransactionHistory history;
 
     public MainMenu() {
         this.userAccount = new BankAccount();
         this.keyboardInput = new Scanner(System.in);
+        this.history = new ViewTransactionHistory();
     }
 
     public void displayOptions() {
         System.out.println("Welcome to the 237 Bank App!");
-        
-        System.out.println("1. Make a deposit");
-        System.out.println("2. Exit the app");
+        System.out.println("1. View transaction history");
+        System.out.println("2. Make a deposit");
+        System.out.println("3. Exit the app");
 
     }
 
     public int getUserSelection(int max) {
         int selection = -1;
-        while(selection < 1 || selection > max) {
+        while (selection < 1 || selection > max) {
             System.out.print("Please make a selection: ");
             selection = keyboardInput.nextInt();
         }
@@ -35,22 +37,34 @@ public class MainMenu {
     public void processInput(int selection) {
         switch (selection) {
             case 1:
+                history.displayTransactionHistory();
+                break;
+            case 2:
                 performDeposit();
         }
     }
 
     public void performDeposit() {
         double depositAmount = -1;
-        while(depositAmount < 0) {
+        while (depositAmount < 0) {
             System.out.print("How much would you like to deposit: ");
             depositAmount = keyboardInput.nextInt();
         }
+
         userAccount.deposit(depositAmount);
+        history.record("Deposited $" + depositAmount);
+    }
+
+    public void displayHistory() {
+        System.out.println("Transaction History:");
+        for (String entry : history.viewHistory()) {
+            System.out.println(entry);
+        }
     }
 
     public void run() {
         int selection = -1;
-        while(selection != EXIT_SELECTION) {
+        while (selection != EXIT_SELECTION) {
             displayOptions();
             selection = getUserSelection(MAX_SELECTION);
             processInput(selection);
